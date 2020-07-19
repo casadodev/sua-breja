e289c61cf4c2ad77c0bfa6c78de53d15d1214b7c
Dagmar Cirino <dagmar.mv@gmail.com>
2020-07-18 22:14:27 -0300

    deploy producion mvp1

---
+++ build/precache-manifest.0287024d32dc815410187a167794bbb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d09b183726f54a196bfac46ab12a558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8187e7992a20772834b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f1424aa2298bd288d84",
+    "url": "/static/js/2.83f40450.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83f40450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8187e7992a20772834b",
+    "url": "/static/js/main.093045f7.chunk.js"
+  },
+  {
+    "revision": "23bb0f2942ca2b15a649",
+    "url": "/static/js/runtime-main.048961ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
