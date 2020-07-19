31e1c0abfdfeeaccec78cfeefe2d59bb6c606443
Dagmar Cirino <dagmar.mv@gmail.com>
2020-07-18 22:42:06 -0300

    corrigindo build

---
+++ build/precache-manifest.8fd8d83844c4364fb2a3ac8eebdf2530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35a93284be945ba7bfd07dac2145de3",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "c39a9edbb283c0a015c1",
+    "url": "/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8f0090fbdca0cdd9976",
+    "url": "/build/static/js/2.134cffcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.134cffcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39a9edbb283c0a015c1",
+    "url": "/build/static/js/main.fc78aaeb.chunk.js"
+  },
+  {
+    "revision": "4803e70dd28104fb465c",
+    "url": "/build/static/js/runtime-main.87af76c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0287024d32dc815410187a167794bbb7.js"
+  "/build/precache-manifest.8fd8d83844c4364fb2a3ac8eebdf2530.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
